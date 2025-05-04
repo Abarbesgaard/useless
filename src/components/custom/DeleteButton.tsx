6d1d06c2f57b1c09f19a84ef39709ba4f719f64c
Andreas B <Abarbesgaard@gmail.com>
2025-05-04 06:41:17 +0200

    alert

--- src/components/custom/DeleteButton.tsx
+++ src/components/custom/DeleteButton.tsx
@@ -1,15 +1,48 @@
 import { Trash2 } from "lucide-react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "../ui/alert-dialog";
 
 interface DeleteButtonProps {
   onClick: () => void;
 }
 
-export const DeleteButton: React.FC<DeleteButtonProps> = ({ onClick }) => (
-  <button
-    onClick={onClick}
-    className="text-red-500 hover:text-red-700 p-3"
-    title="Delete"
-  >
-    <Trash2 size={16} />
-  </button>
-);
+export const DeleteButton: React.FC<DeleteButtonProps> = ({ onClick }) => {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <button className="text-red-500 hover:text-red-700 p-3" title="Delete">
+          <Trash2 size={16} />
+        </button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={() => {
+              console.log("Delete confirmed");
+              onClick();
+            }}
+          >
+            Continue
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
